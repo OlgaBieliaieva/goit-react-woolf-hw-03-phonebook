39d554c4851e9c8fd464f53b171eb7ec952738fa
OlgaBieliaieva <belyaevaolya0@gmail.com>
2024-03-11 21:51:23 +0200

    fixed

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -3,21 +3,19 @@ import css from './ContactList.module.css';
 const ContactList = ({ contacts, onDeleteContact }) => {
   return contacts.length > 0 ? (
     <ul className={css.contactList}>
-      {contacts.map(({ id, name, number }) => {
-        return (
-          <li className={css.contactItem} key={id}>
-            {name}: {number}
-            <button
-              className={css.listItemBtn}
-              id={id}
-              type="button"
-              onClick={() => onDeleteContact(id)}
-            >
-              Delete
-            </button>
-          </li>
-        );
-      })}
+      {contacts.map(({ id, name, number }) => (
+        <li className={css.contactItem} key={id}>
+          {name}: {number}
+          <button
+            className={css.listItemBtn}
+            id={id}
+            type="button"
+            onClick={() => onDeleteContact(id)}
+          >
+            Delete
+          </button>
+        </li>
+      ))}
     </ul>
   ) : (
     <p className={css.contactListDefault}>
